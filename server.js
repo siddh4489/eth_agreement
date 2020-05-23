491b0fab3bcde9e44e45e6c3d02beb39d9a95548
siddh4489 <siddh4489@gmail.com>
2020-05-24 02:07:09 +0530

    Update server.js
    change the server

--- server.js
+++ server.js
@@ -22,8 +22,5 @@ const routes = require('./routes');
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000, err => {
-    if (err) throw err;
-    console.log('Ready on localhost:3000');
-  });
+  createServer(handler).listen(process.env.PORT);
 });
