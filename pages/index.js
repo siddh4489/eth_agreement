608b704767af4668bb95bc02fe4c96b921b45e9c
siddh4489 <siddh4489@gmail.com>
2020-06-02 18:27:52 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import Layout from '../components/Layout';
-import  {Home}  from './Home';
+import  Home from './Home';
 
 class App extends React.Component {
   render() {
